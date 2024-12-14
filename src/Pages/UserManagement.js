cfe8894531684e16d6712d53dab75d300c39c988
vaniiaa <vanivania2005@gmail.com>
2024-12-14 20:43:52 +0700

    update

--- src/Pages/UserManagement.js
+++ src/Pages/UserManagement.js
@@ -165,8 +165,8 @@ function UserManagement() {
     );
 
     return (
-        <Box sx={{ p: 2 }}>
-            <Paper elevation={3} sx={{ borderRadius: '8px', p: 3 }}>
+        <Box sx={{ p: 1 }}>
+            <Paper elevation={3} sx={{ borderRadius: '8px', p: 3, mt:-8 }}>
                 <Typography
                     variant="h5"
                     sx={{ mb: 1, display: 'flex', alignItems: 'center', color: '#0055A8', fontWeight: 'bold' }}
